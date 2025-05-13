ce36ff80854353cfce3bdd7d5cd591c1a3ebb81a
ziyamohammad <mohammad2311061@akgec.ac.in>
2025-05-13 22:38:45 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+       <div className="logo">
+        <img src="./new logo.png" alt="/" height="100%" width="100%"/>
+        <div className="top-right">
+          <img src="./top-right.png" alt=""  height="100%" width="100%"/>
+        </div>
+         <div className="bottom-right">
+          <img src="./bottom-right.png" alt="" height="100%" width="100%" />
+        </div>
+        <div className="top-left">
+          <img src="./top-left.png" alt="" height="100%" width="100%" />
+        </div>
+         <div className="top-left-ball">
+          <img src="./top-left-ball.png" alt="" height="100%" width="100%" />
+        </div>
+        <div className="top-left-ball2">
+          <img src="./top-left-ball2.png" alt="" height="100%" width="100%" />
+        </div>
+         <div className="bottom-left">
+          <img src="./bottom-left.png" alt="" height="100%" width="100%" />
+        </div>
+         <div className="bottom-left-ball">
+          <img src="./bottom-left-ball.png" alt="" height="100%" width="100%" />
+        </div>
+         <div className="bottom-left-ball2">
+          <img src="./bottom-left-ball2.png" alt="" height="100%" width="100%" />
+        </div>
+      </div>
+      <h1>Trained & Tuned'25</h1>
+      <h2 className="coming">Coming Soon!</h2>
+      <div className="query">
+      <span>For Any Query</span>
+      <span className="number">
+        <p>8052587573(Om)</p>
+        <p>9917741948(Devroop)</p>
+      </span>
+      </div>
     </div>
   );
 }
